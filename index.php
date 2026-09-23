6cb3a8e6f17491b766b4219696c0fa9e0fc22aca
imorenoca <153509984+imorenoca@users.noreply.github.com>
2026-09-23 09:22:59 +0200

    featere: corrección errores index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ $rutas = [
     '/' => __DIR__ .'/vistas/bienvenida.php',
 ];
 
-$ruta_silicitada = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$ruta_solicitada = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if (array_key_exists($ruta_silicitada, $rutas)) {
-    require_once $rutas[$ruta_silicitada];
+if (array_key_exists($ruta_solicitada, $rutas)) {
+    require_once $rutas[$ruta_solicitada];
 } else {
     require_once __DIR__ . '/vistas/error404.php';
 }
